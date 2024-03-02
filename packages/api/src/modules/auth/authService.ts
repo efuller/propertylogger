4ca6565eec78cf9bf6d3711f75e42356c175b119
Eric Fuller <info@glassmoonmedia.com>
2024-03-02 09:28:43 -0500

    Update authService interface

--- packages/api/src/modules/auth/authService.ts
+++ packages/api/src/modules/auth/authService.ts
@@ -1,7 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
 
 export interface AuthService {
-  getAudience(): string;
-  getIssuerBaseURL(): string;
   authorize(req: Request, res: Response, next: NextFunction): void;
 }
