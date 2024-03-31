cbeabf0ede0c7213294821f2a33612c3cf4fded8
Eric Fuller <info@glassmoonmedia.com>
2024-03-31 09:08:07 -0400

    Woohoo -- write a test for creating a journal and updating the vm

--- packages/web/src/modules/jounals/journal.spec.ts
+++ packages/web/src/modules/jounals/journal.spec.ts
@@ -1,4 +1,4 @@
-import { MockApi, MockApiClient } from '../../shared/apiClient/apiClient.ts';
+import { MockApiClient } from '../../shared/apiClient/apiClient.ts';
 
 const mockAuthClient = jest.fn(() => ({
   loginWithRedirect: jest.fn(),
@@ -53,5 +53,34 @@ describe('Journal', () => {
 
     // Assert
     expect(presenter.viewModel.journals.length).toBe(2);
+  });
+
+  it('should create a new journal and add it to the list', async () => {
+    const { presenter, controller } = compositionRoot.getJournalModule();
+    const apiClient = compositionRoot.getApiClient() as MockApiClient;
+    apiClient.setPostResponse({
+      success: true,
+      error: false,
+      data: {
+        id: '3',
+        title: 'Journal 3',
+        content: 'Journal 3 content',
+      },
+    });
+
+    if (!presenter || !controller) {
+      throw new Error('Journal presenter / controller not set up');
+    }
+
+    expect(presenter.viewModel.journals.length).toBe(0);
+
+    // Act
+    await controller.createJournal({
+      title: 'Journal 3',
+      content: 'Journal 3 content',
+    });
+
+    // Assert
+    expect(presenter.viewModel.journals.length).toBe(1);
   });
 });
