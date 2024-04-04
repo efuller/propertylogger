fb77cffddfd7bffc7240cc229b445b076b1a7946
Eric Fuller <info@glassmoonmedia.com>
2024-04-03 20:11:01 -0400

    Move auth0Adapter.ts into shared folder

--- packages/web/src/modules/auth/authClient.ts
+++ packages/web/src/modules/auth/authClient.ts
@@ -1,4 +1,3 @@
-import { Auth0Client } from '@auth0/auth0-spa-js';
 export interface AuthClient {
   login(): Promise<void>;
   logout(): Promise<void>;
@@ -6,60 +5,4 @@ export interface AuthClient {
   getToken(): Promise<string>;
   isAuthenticated(): Promise<boolean>;
   checkSession(): Promise<void>;
-}
-
-export class Auth0Adapter implements AuthClient {
-  constructor(private auth0Client: Auth0Client) {
-    this.auth0Client = auth0Client;
-  }
-
-  public async login(): Promise<void> {
-    await this.auth0Client.loginWithRedirect();
-  }
-
-  public async logout(): Promise<void> {
-    await this.auth0Client.logout();
-  }
-
-  public async handleRedirectCallback(): Promise<void> {
-    await this.auth0Client.handleRedirectCallback();
-  }
-
-  public async getToken(): Promise<string> {
-    return await this.auth0Client.getTokenSilently();
-  }
-
-  public async isAuthenticated(): Promise<boolean> {
-    return this.auth0Client.isAuthenticated();
-  }
-
-  public async checkSession(): Promise<void> {
-    await this.auth0Client.checkSession();
-  }
-}
-
-export class MockAuth0Adapter implements AuthClient {
-  public async login(): Promise<void> {
-    return;
-  }
-
-  public async logout(): Promise<void> {
-    return;
-  }
-
-  public async handleRedirectCallback(): Promise<void> {
-    return;
-  }
-
-  public async getToken(): Promise<string> {
-    return 'mock-token';
-  }
-
-  public async isAuthenticated(): Promise<boolean> {
-    return true;
-  }
-
-  public async checkSession(): Promise<void> {
-    return Promise.resolve(undefined);
-  }
 }
--- packages/web/src/shared/compositionRoot/compositionRoot.tsx
+++ packages/web/src/shared/compositionRoot/compositionRoot.tsx
@@ -6,8 +6,9 @@ import { JournalRepo } from '../../modules/jounals/journal.repo.ts';
 import { JournalPresenter } from '../../modules/jounals/journal.presenter.ts';
 import { ApiClient, FetchApiClient, MockApi, MockApiClient } from '../apiClient/apiClient.ts';
 import { JournalController } from '../../modules/jounals/journal.controller.ts';
-import { Auth0Adapter, AuthClient } from '../../modules/auth/authClient.ts';
+import { AuthClient } from '../../modules/auth/authClient.ts';
 import { Auth0Client, createAuth0Client } from '@auth0/auth0-spa-js';
+import { Auth0Adapter } from '../auth/auth0Adapter.ts';
 
 export class CompositionRoot {
   router: AppRouter | undefined;
