b568edac13384cf7c9e1af8119774d8f62c22c1c
Eric Fuller <info@glassmoonmedia.com>
2024-05-14 20:51:06 -0400

    Get 'creating account' working ... in theory

---
+++ packages/web/src/pages/creatingAccount.page.tsx
@@ -0,0 +1,7 @@
+export const CreatingAccountPage = () => {
+  return (
+    <div>
+      <h1>Creating Account...</h1>
+    </div>
+  );
+}
--- packages/web/src/shared/router/index.tsx
+++ packages/web/src/shared/router/index.tsx
@@ -1,4 +1,6 @@
 import { redirect, RouteObject } from 'react-router-dom';
+import * as jose from 'jose';
+
 import { HomePage } from '../../pages/home.page.tsx';
 import { LoggingInPage } from '../../pages/loggingIn.page.tsx';
 import { DashboardPage } from '../../pages/app/dashboard.page.tsx';
@@ -8,6 +10,15 @@ import { JournalsPage } from '../../pages/app/journals/journals.page.tsx';
 import { JournalController } from '../../modules/jounals/journal.controller.ts';
 import { JournalPresenter } from '../../modules/jounals/journal.presenter.ts';
 import { NotFoundPage } from '../../pages/404/404.page.tsx';
+import { CreatingAccountPage } from '../../pages/creatingAccount.page.tsx';
+
+interface JWTPayload {
+  data: {
+    user: {
+      user_id: string;
+    };
+  };
+}
 
 const CODE_RE = /[?&]code=[^&]+/;
 const STATE_RE = /[?&]state=[^&]+/;
@@ -44,6 +55,46 @@ export class AppRouter {
     return [
       {
         path: '/creating-account',
+        element: <CreatingAccountPage />,
+        loader: async (item) => {
+          if (!process.env.AUTH0_DOMAIN) {
+            throw new Error('AUTH0_DOMAIN is not set');
+          }
+
+          if (!process.env.APP_SECRET_KEY) {
+            throw new Error('APP_SECRET_KEY is not set');
+          }
+
+          console.log('ITEM', item);
+          const url = new URL(item.request.url);
+          const token = url.searchParams.get('session_token');
+          const state = url.searchParams.get('state');
+
+          if (!token) {
+            return redirect('/');
+          }
+
+          const secret = new TextEncoder().encode(process.env.APP_SECRET_KEY);
+          const decoded: jose.JWTVerifyResult<JWTPayload> = await jose.jwtVerify(token, secret);
+          console.log('DECODED', decoded);
+
+          const created = { state, memberCreate: true };
+          const alg = 'HS256'
+          const jwt = await new  jose.SignJWT(created)
+            .setIssuedAt()
+            .setSubject(decoded.payload.data.user.user_id)
+            .setProtectedHeader({ alg, typ: 'JWT' })
+            .setIssuer(process.env.AUTH0_DOMAIN)
+            .setExpirationTime('2h')
+            .sign(secret);
+
+          const continueUri = `${decoded.payload.continue_uri}?state=${state}&session_token=${jwt}`;
+
+          return redirect(continueUri);
+        }
+      },
+      {
+        path: '/logging-in',
         element: <LoggingInPage />,
         loader: async () => {
           await this.authController.isAuthenticated();
