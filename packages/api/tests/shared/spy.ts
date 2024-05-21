af50141b76ae644d3d66e11812cf9205d1795b8e
Eric Fuller <info@glassmoonmedia.com>
2024-05-21 07:33:29 -0400

    Write a basic spy that can be reused

--- packages/api/tests/shared/spy.ts
+++ packages/api/tests/shared/spy.ts
@@ -3,3 +3,30 @@ export type Call = {
   calledBy?: string;
 }
 
+// TODO: Utilize this class for other spy's in the codebase
+export abstract class Spy {
+  calls: Record<string, Call[]> = {};
+
+  addCall(methodName: string, data: Call) {
+    if (!this.calls[methodName]) {
+      this.calls[methodName] = [{ ...data }];
+      return;
+    }
+    this.calls[methodName].push({ ...data });
+  }
+
+  timesCalled(methodName: string) {
+    if (!this.calls[methodName]) {
+      return 0;
+    }
+    return this.calls[methodName].length;
+  }
+
+  toHaveBeenCalledBy(methodName: string, calledBy: string) {
+    if (!this.calls[methodName]) {
+      return false;
+    }
+    return this.calls[methodName].some((call) => call.calledBy === calledBy);
+  }
+}
+
