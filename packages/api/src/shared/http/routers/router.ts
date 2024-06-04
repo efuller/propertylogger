11631466315eeecc0c2aafde82c46eede081d7af
Eric Fuller <info@glassmoonmedia.com>
2024-06-03 20:06:51 -0400

    Remove router abstraction

--- packages/api/src/shared/http/routers/router.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-// What do we need to do in order to require
-// constructor parameters for a class?
-// Static create? Abstract class?
-export interface ApiRouter {
-  getRouter(): express.Router;
-  setupRoutes(): void;
-}
