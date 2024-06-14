baed855203fbb93f82f43020415bb606a5b5102b
Eric Fuller <info@glassmoonmedia.com>
2024-06-13 20:51:42 -0400

    Adjust get request for API driver

--- packages/api/src/shared/http/restApiDriver.ts
+++ packages/api/src/shared/http/restApiDriver.ts
@@ -5,11 +5,26 @@ import { ApiResponse } from '@efuller/shared/src/api';
 export class RestApiDriver {
   constructor(private http: Server) {}
 
-  async get(path: string, headers: Record<string, string> = {}) {
-    const response =  request(this.http)
-      .get(path)
-      .set(headers);
-    return response;
+  async get<Result>(path: string, headers: Record<string, string> = {}): Promise<ApiResponse<Result>> {
+    try {
+      const response = await request(this.http)
+        .get(path)
+        .set(headers);
+
+      if (!response.ok) {
+        if (response.error && response.error.message) {
+          throw new Error(response.error.message);
+        }
+      }
+
+      return response.body;
+    } catch (error) {
+      return {
+        success: false,
+        data: undefined,
+        error: error as Error,
+      };
+    }
   }
 
   async post<ApiRequest extends object, TData>(path: string, data: ApiRequest): Promise<ApiResponse<TData>> {
