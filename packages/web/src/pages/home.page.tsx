9d2c728952560bc94c55c466cf48e62f70b95ccd
Eric Fuller <info@glassmoonmedia.com>
2024-06-19 19:02:33 -0400

    Get a basic landing page setup

--- packages/web/src/pages/home.page.tsx
+++ packages/web/src/pages/home.page.tsx
@@ -1,4 +1,5 @@
 import { compositionRoot } from '../shared/compositionRoot/compositionRoot.tsx';
+import { Button } from '@/components/ui/button.tsx';
 
 export const HomePage = () => {
   const { controller } = compositionRoot.getAuthModule();
@@ -8,12 +9,12 @@ export const HomePage = () => {
   }
 
   return (
-    <>
-      <div>New HomePage</div>
-      <div>
-        <h1>Welcome to PropertyLogger</h1>
-        <button id="login" onClick={handleLogin}>Login</button>
+      <div className="container mx-auto grid h-screen justify-center items-center">
+        <div className="flex justify-center items-center flex-col gap-12 font-extrabold">
+          <h1 className="text-6xl">Welcome to PropertyLogger</h1>
+          <img className="max-h-[350px]" src="pl-logo.png" alt="PropertyLogger Logo" />
+          <Button id="login" onClick={handleLogin}>Login</Button>
+        </div>
       </div>
-    </>
   );
 }
