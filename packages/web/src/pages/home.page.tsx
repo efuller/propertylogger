981cab2864deecb9d2d2862cab0783609ed9f98d
Eric Fuller <info@glassmoonmedia.com>
2024-06-20 21:16:45 -0400

    Tweak mobile styles

--- packages/web/src/pages/home.page.tsx
+++ packages/web/src/pages/home.page.tsx
@@ -11,7 +11,7 @@ export const HomePage = () => {
   return (
       <div className="container mx-auto grid h-screen justify-center items-center">
         <div className="flex justify-center items-center flex-col gap-12 font-extrabold">
-          <h1 className="text-6xl">Welcome to PropertyLogger</h1>
+          <h1 className="text-3xl lg:text-6xl text-center">Welcome to PropertyLogger</h1>
           <img className="max-h-[350px]" src="pl-logo.png" alt="PropertyLogger Logo" />
           <Button id="login" onClick={handleLogin}>Login</Button>
         </div>
